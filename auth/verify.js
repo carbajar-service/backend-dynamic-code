b2f5fdc3d14a9c9ba2a7508975a6cfe0eb95607c
usman <usmanpasha002@gmail.com>
2025-11-17 18:44:27 +0530

    driver token issue fix

--- auth/verify.js
+++ auth/verify.js
@@ -3,6 +3,7 @@ const config = require("../config/index.js");
 const userService = require("../services/user.services.js");
 const staffService = require("../services/staff.services.js");
 const adminModel = require("../models/admin.model.js");
+const driverModel = require("../models/driver.model.js");
 const logger = require("../utils/logs.js");
 const AppError = require("../utils/appError.js")
 const { catchError } = require("../utils/catchError.js")
@@ -25,10 +26,11 @@ module.exports.verifyJWT = catchError(async (req, _, next) => {
         // Check if the decoded ID exists in the user collection
         const userPromise = userService.findOneRecord(decodedToken?._id);
         const staffPromise = staffService.findOneRecord(decodedToken?._id);
+        const driverPromise = driverModel.findOne({ _id: decodedToken?._id });
         // Check if the decoded ID exists in the admin collection
         const adminPromise = adminModel.findOne({ _id: decodedToken?._id });
 
-        const [user, admin, staff] = await Promise.all([userPromise, adminPromise, staffPromise]);
+        const [user, admin, staff, driver] = await Promise.all([userPromise, adminPromise, staffPromise, driverPromise]);
 
         if (user) {
             logger.info(`User found: ${JSON.stringify(user)}`);
@@ -46,9 +48,14 @@ module.exports.verifyJWT = catchError(async (req, _, next) => {
             req.admin = admin;
             req.userId = admin._id; // Using userId for uniformity
         }
+        if (driver) {
+            logger.info(`Driver found: ${JSON.stringify(driver)}`);
+            req.driver = driver;
+            req.userId = driver._id; // Using userId for uniformity
+        }
 
         // If neither user nor admin is found, throw an error
-        if (!user && !admin && !staff) {
+        if (!user && !admin && !staff && !driver) {
             logger.warn("No user or admin found for token");
             throw new AppError(401, "Invalid Access Token");
         }
